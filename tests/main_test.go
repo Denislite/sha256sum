c2dc9f9aafcb3b56a04087c0af68619e087538b9
Denislite <danson7850@gmail.com>
2022-04-07 16:05:48 +0300

    tests update

---
+++ tests/main_test.go
@@ -0,0 +1,15 @@
+package tests
+
+import (
+	"testing"
+)
+
+const testPath = "path"
+
+func BenchmarkConcurrency(b *testing.B) {
+	b.ResetTimer()
+
+	//create normal tests, not this shit
+
+	b.StopTimer()
+}
