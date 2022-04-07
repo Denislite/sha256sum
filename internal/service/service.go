17acd85906e3a84d1177f3f3923d888809593bbf
Denislite <danson7850@gmail.com>
2022-04-07 16:10:23 +0300

    little fix :)

--- internal/service/service.go
+++ internal/service/service.go
@@ -49,7 +49,7 @@ func createDocs() {
 		flag.VisitAll(func(f *flag.Flag) {
 			_, err := fmt.Fprintf(os.Stderr, "-%v %v  \n", f.Name, f.Usage)
 			if err != nil {
-				log.Println(domain.ErrorOption)
+				log.Println(domain.ErrorDocs)
 				return
 			}
 		})
