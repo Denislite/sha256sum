02eccf68cbb3c41f411ee1595e3362935697ca64
Dzianis Lohvinau <dlohvinau@scnsoft.com>
2023-05-04 15:28:06 +0300

    Update schema and .env example

--- schema/000001_sql.up.sql
+++ schema/000001_sql.up.sql
@@ -1,7 +1,8 @@
 CREATE TABLE files
 (
-    id              BIGSERIAL PRIMARY KEY,
+    id              UUID PRIMARY KEY,
     pod_name        VARCHAR,
+    container_id    UUID,
     image_name      VARCHAR,
     image_version   VARCHAR,
     file_name       VARCHAR,
@@ -10,5 +11,46 @@ CREATE TABLE files
     hash_type       VARCHAR,
     created_at      TIMESTAMP DEFAULT now(),
 
-    CONSTRAINT files_unique UNIQUE (file_path, hash_type)
+    CONSTRAINT files_unique UNIQUE (file_path, hash_type),
+    FOREIGN KEY (container_id) REFERENCES containers (id)
+);
+
+CREATE TABLE containers
+(
+    id UUID PRIMARY KEY,
+    container_name VARCHAR,
+    image_name VARCHAR,
+    image_version VARCHAR
+);
+
+CREATE TABLE verifications
+(
+    id UUID PRIMARY KEY,
+    info_id UUID,
+    container_name VARCHAR,
+    image_name VARCHAR,
+    image_version VARCHAR,
+    FOREIGN KEY (info_id) REFERENCES verificationInfo (id)
+);
+
+CREATE TABLE verificationInfo
+(
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP,
+    status VARCHAR
+);
+
+CREATE TABLE verificationFiles
+(
+    id_verification UUID PRIMARY KEY,
+    id_file UUID PRIMARY KEY,
+    FOREIGN KEY (id_file) REFERENCES fileInfo (id),
+    FOREIGN KEY (id_verification) REFERENCES verificationInfo (id)
+);
+
+CREATE TABLE fileInfo
+(
+    id UUID PRIMARY KEY,
+    file_name       VARCHAR,
+    hash_value      VARCHAR,
 );
